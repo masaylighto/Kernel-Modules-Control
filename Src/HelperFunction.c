4549dfc2e64135340ddbaf32425935af1f0417f4
Masaylighto <www.masaylighto@gmail.com>
2022-05-09 19:32:25 +0300

    remove modeprobe copyed functions

--- Src/HelperFunction.c
+++ Src/HelperFunction.c
@@ -8,49 +8,6 @@ bool UnloadModule(const char * FilePath){
     return syscall(SYS_delete_module,FilePath,(O_NONBLOCK | O_TRUNC))==0;
 }
 
-static void *grab_file(const char *filename, unsigned long *size)
-{
-	unsigned int max = 16384;
-	int ret, fd, err_save;
-	void *buffer;
-
-	if (strcmp(filename, "-") == 0)
-		fd = dup(STDIN_FILENO);
-	else
-		fd = open(filename, O_RDONLY, 0);
-
-	if (fd < 0)
-		return NULL;
-
-	buffer = malloc(max);
-	if (!buffer)
-		goto out_error;
-
-	*size = 0;
-	while ((ret = read(fd, buffer + *size, max - *size)) > 0) {
-		*size += ret;
-		if (*size == max) {
-			void *p;
-
-			p = realloc(buffer, max *= 2);
-			if (!p)
-				goto out_error;
-			buffer = p;
-		}
-	}
-	if (ret < 0)
-		goto out_error;
-
-	close(fd);
-	return buffer;
-
-out_error:
-	err_save = errno;
-	free(buffer);
-	close(fd);
-	errno = err_save;
-	return NULL;
-}
 //Read File Binary
  void * ReadFileBytes(const char * FilePath,long FileSize)
  {
